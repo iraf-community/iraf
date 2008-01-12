586bc8d774649215ee6cf60d9fdae6cdc11f47a6
cyamauch <cyamauch@0682cb55-6b43-0410-8320-9b94b4336a91>
2008-01-12 10:22:12 +0000

    Added iraf.lp64.h and mach.lp64.h.
    git-svn-id: https://svn.code.sf.net/p/iraf64/code/trunk/src@9 0682cb55-6b43-0410-8320-9b94b4336a91

--- iraf/unix/config/mach.lp64.h
+++ iraf/unix/config/mach.lp64.h
@@ -11,13 +11,13 @@ define	MAX_EXPONENTR	38		# IEEE single
 define	MAX_EXPONENTD	308		# IEEE double
 
 define	MAX_SHORT	32767		# largest numbers
-define	MAX_INT		9223372036854775807
+define	MAX_INT		2147483647
 define	MAX_LONG	9223372036854775807
 define	MAX_REAL	0.99e37		# anything larger is INDEF
 define	MAX_DOUBLE	0.99d307
 define	NBITS_BYTE	8		# nbits in a machine byte
 define	NBITS_SHORT	16		# nbits in a short	
-define	NBITS_INT	64		# nbits in an integer	
+define	NBITS_INT	32		# nbits in an integer	
 define	EPSILONR	(1.192e-7)	# smallest E such that 1.0 + E > 1.0
 define	EPSILOND	(2.220d-16)	# double precision epsilon
 define	EPSILON		EPSILONR
