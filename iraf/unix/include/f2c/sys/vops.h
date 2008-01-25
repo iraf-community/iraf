61c2d797f884813d5a03befe2c8441baff089760
cyamauch <cyamauch@0682cb55-6b43-0410-8320-9b94b4336a91>
2008-01-25 08:51:45 +0000

    Appended a missing update for Rev.89.
    git-svn-id: https://svn.code.sf.net/p/iraf64/code/trunk/src@91 0682cb55-6b43-0410-8320-9b94b4336a91

--- iraf/unix/include/f2c/sys/vops.h
+++ iraf/unix/include/f2c/sys/vops.h
@@ -3,6 +3,5 @@
 /* libvops.a */
 #include "../../../../sys/vops/ak/f2c_proto.h"
 #include "../../../../sys/vops/lz/f2c_proto.h"
-#include "../../../../sys/vops/achtgen/f2c_proto.h"
 #include "../../../../sys/vops/f2c_proto.h"
 #include "../../../../sys/osb/f2c_proto.h"
