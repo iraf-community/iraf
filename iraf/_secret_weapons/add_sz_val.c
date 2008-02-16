1c8b2c72f2388b85c949f5ff77d6fd2c55f5e75f
cyamauch <cyamauch@0682cb55-6b43-0410-8320-9b94b4336a91>
2008-02-16 13:44:34 +0000

    Added more strict parser (add_sz_val.c).
    git-svn-id: https://svn.code.sf.net/p/iraf64/code/trunk/src@163 0682cb55-6b43-0410-8320-9b94b4336a91

--- iraf/_secret_weapons/add_sz_val.c
+++ iraf/_secret_weapons/add_sz_val.c
@@ -67,6 +67,56 @@ static int is_valchar( int ch )
     return ( isalpha(ch) || isdigit(ch) || ch=='_' );
 }
 
+static int parse_in_braces ( const char *char_ptr_in_brace, char *lines[], 
+			     int current_line_idx, int max_line_idx,
+			     const char *end_chars, const char **end_ptr )
+{
+    const char *ip;
+    int br_in_0 = 0;	/* [] */
+    int br_in_1 = 0;	/* () */
+    bool qt_in_0 = false;	/* "" */
+    bool qt_in_1 = false;	/* '' */
+    bool flg_complete_line = false;
+    int j = current_line_idx;
+    for ( ip=char_ptr_in_brace ; j <= max_line_idx ; ip=lines[++j] ) {
+	while ( *ip != '\n' && *ip != '\0' ) {
+	    if ( qt_in_0 == true && *ip==0x5c )
+		ip++;
+	    else if ( qt_in_1 == true && *ip==0x5c )
+		ip++;
+	    else if ( qt_in_0 == true && *ip=='"' )
+		qt_in_0=false;
+	    else if ( qt_in_1 == true && *ip==0x27 )
+		qt_in_1=false;
+	    else {
+		if ( qt_in_0 == false && qt_in_1 == false ) {
+		    if ( *ip == '#' ) break;
+		    else if ( *ip == '"' ) qt_in_0=true;
+		    else if ( *ip == 0x27 ) qt_in_1=true;
+		    else if ( strchr(end_chars,*ip)!=NULL &&
+			      br_in_0 == 0 && br_in_1 == 0 ) {
+			flg_complete_line = true;
+			break;
+		    }
+		    else if ( *ip == '[' ) br_in_0++;
+		    else if ( *ip == '(' ) br_in_1++;
+		    else if ( *ip == ']' ) br_in_0--;
+		    else if ( *ip == ')' ) br_in_1--;
+		}
+	    }
+	    ip++;
+	}
+	if ( flg_complete_line == true ) break;
+    }
+
+    if ( end_ptr != NULL ) *end_ptr = ip;
+
+    if ( flg_complete_line == true ) return j;
+    else return -1;
+}
+
+#define FLG_IF_WITHOUT_BRACE 1
+
 static int add_sz_val( const char *proc_name, int target_arg, 
 		       const char *ap_type, const char *ap_val,
 		       const char *file_name )
@@ -76,6 +126,7 @@ static int add_sz_val( const char *proc_name, int target_arg,
     FILE *fp = NULL;
     int num_lines = 0;
     char **lines = NULL;
+    int *flags = NULL;
     int num_proc;
     bool needs_update = false;
     /* line indices */
@@ -101,7 +152,14 @@ static int add_sz_val( const char *proc_name, int target_arg,
 	    goto quit;
 	}
 	lines = tmp_ptr;
+	tmp_ptr = realloc(flags,sizeof(int)*(num_lines+1));
+	if ( tmp_ptr == NULL ) {
+	    fprintf(stderr,"[ERROR] realloc() failed\n");
+	    goto quit;
+	}
+	flags = tmp_ptr;
 	lines[num_lines] = strdup(line_buf);
+	flags[num_lines] = 0;
 	if ( lines[num_lines] == NULL ) {
 	    fprintf(stderr,"[ERROR] strdup() failed\n");
 	    goto quit;
@@ -243,14 +301,68 @@ static int add_sz_val( const char *proc_name, int target_arg,
 	int  prev_line_idx = -1;
 	int j, j_not_gc;
 	bool target_found = false;
-	bool flg_if_or_else = false;
 	j_not_gc = -1;
+
+	/* check if,else part */
+	for ( j=proc_begin[i]+1 ; j < proc_end[i] ; j++ ) {
+	    const char *ip;
+	    const char *ip1;
+	    ip = lines[j];
+	    while ( *ip == ' ' || *ip == '\t' ) ip++;
+	    if ( *ip == '#' || *ip == '\n' || *ip == '$' ) continue;
+	    if ( *ip == '\0' ) continue;
+	    /* */
+	    for ( ip1=ip ; ; ) {
+		const char *ip2 = NULL;
+		while ( isalpha(*ip1)==0 && *ip1!='\0' ) ip1++;
+		if ( (strncmp(ip1,"if",2)==0 && is_valchar(ip1[2])==0) ) ip2=ip1+2;
+		if ( (strncmp(ip1,"else",4)==0 && is_valchar(ip1[4])==0) ) ip2=ip1+4;
+		if ( (strncmp(ip1,"while",5)==0 && is_valchar(ip1[5])==0) ) ip2=ip1+5;
+		if ( (strncmp(ip1,"do",2)==0 && is_valchar(ip1[2])==0) ) ip2=ip1+2;
+		if ( (strncmp(ip1,"for",3)==0 && is_valchar(ip1[3])==0) ) ip2=ip1+3;
+		if ( (strncmp(ip1,"repeat",6)==0 && is_valchar(ip1[6])==0) ) ip2=ip1+6;
+		if ( (strncmp(ip1,"iferr",5)==0 && is_valchar(ip1[5])==0) ) ip2=ip1+5;
+		if ( (strncmp(ip1,"ifnoerr",7)==0 && is_valchar(ip1[7])==0) ) ip2=ip1+7;
+		if ( ip2 == NULL ) {
+		    break;
+		}
+		else {
+		    while ( *ip2 == ' ' || *ip2 == '\t' ) ip2++;
+		    if ( *ip2 == '\n' || *ip2 == '#'|| *ip2 == '\0' ) {
+			flags[j] |= FLG_IF_WITHOUT_BRACE;
+			break;
+		    }
+		    else if ( *ip2 == '{' ) {
+			break;
+		    }
+		    else if ( *ip2 == '(' ) {	/* brace begins... */
+			int o_j = j;
+			const char *ip3;
+			j = parse_in_braces ( ip2+1, lines, j, proc_end[i]-1, ")", &ip3 );
+			if ( j < 0 ) {
+			    fprintf(stderr,"[ERROR] file = %s  line = %d: Cannot parse\n",
+				    file_name,o_j+1);
+			    goto quit;
+			}
+			ip3++;
+			while ( *ip3 == ' ' || *ip3 == '\t' ) ip3++;
+			if ( *ip3 != '{' ) {
+			    flags[j] |= FLG_IF_WITHOUT_BRACE;
+			}
+			break;
+		    }
+		}
+		ip1 = ip2;
+	    }
+	}
+
+	/* */
 	for ( j=proc_begin[i]+1 ; j < proc_end[i] ; j++ ) {
 	    const char *ip;
 	    const char *ptr_call_begin;
 	    const char *ptr_proc_begin;
 	    int arg_cnt = 0;
-	    bool flg_prev_if;
+	    bool flg_prev_if_without_brace;
 	    int prev_j;
 	    if ( DEBUG ) {
 		fprintf(stderr,"debug: j = %d\n",j);
@@ -259,29 +371,14 @@ static int add_sz_val( const char *proc_name, int target_arg,
 	    while ( *ip == ' ' || *ip == '\t' ) ip++;
 	    if ( *ip == '#' || *ip == '\n' || *ip == '$' ) continue;
 	    if ( *ip == '\0' ) continue;
-	    /* */
-	    flg_prev_if = flg_if_or_else;
+	    /* previous valid line */
 	    prev_j = j_not_gc;
 	    /* */
-	    /* detect if,else,etc. */
-	    flg_if_or_else = false;
-	    {
-		const char *ip1 = ip;
-		j_not_gc = j;
-		while ( isalpha(*ip1)==0 && *ip1!='\0' ) ip1++;
-		if ( (strncmp(ip1,"if",2)==0 && is_valchar(ip1[2])==0) ||
-		     (strncmp(ip1,"else",4)==0 && is_valchar(ip1[4])==0) ||
-		     (strncmp(ip1,"while",5)==0 && is_valchar(ip1[5])==0) ||
-		     (strncmp(ip1,"do",2)==0 && is_valchar(ip1[2])==0) ||
-		     (strncmp(ip1,"for",3)==0 && is_valchar(ip1[3])==0) ||
-		     (strncmp(ip1,"repeat",6)==0 && is_valchar(ip1[6])==0) ||
-		     (strncmp(ip1,"iferr",5)==0 && is_valchar(ip1[5])==0) ||
-		     (strncmp(ip1,"ifnoerr",7)==0 && is_valchar(ip1[7])==0) ) {
-		    if ( strrchr(ip1,'{') == NULL ) {
-			flg_if_or_else = true;
-		    }
-		}
-	    }
+	    if ( 0 <= prev_j && (flags[prev_j] & FLG_IF_WITHOUT_BRACE) )
+		flg_prev_if_without_brace = true;
+	    else flg_prev_if_without_brace = false;
+	    /* Register valid line */
+	    j_not_gc = j;
 	    /* */
 	    if ( strncmp(ip,"call ",5) != 0 && strncmp(ip,"call\t",5) != 0 ) {
 		continue;
@@ -296,7 +393,14 @@ static int add_sz_val( const char *proc_name, int target_arg,
 	    while ( *ip == ' ' || *ip == '\t' ) ip++;
 	    if ( *ip != '(' ) continue;
 	    /* */
-	    if ( flg_prev_if == true ) {
+	    if ( flg_prev_if_without_brace == true && 
+		 parse_in_braces (ip+1, lines, j, proc_end[i]-1, ")", NULL) != j ) {
+		fprintf(stderr,"[ERROR] file = %s  line = %d: Cannot handle\n",
+			file_name,j+1);
+		goto quit;
+	    }
+	    /* */
+	    if ( flg_prev_if_without_brace == true ) {
 		char tmp_buf[SZ_LINE_BUF];
 		const char *ip1;
 		char *op1;
@@ -407,14 +511,10 @@ static int add_sz_val( const char *proc_name, int target_arg,
 		fprintf(stderr,"debug: 1st arg begins...\n");
 	    }
 	    do {
-		const char *ip1;
 		const char *next_arg = NULL;
 		const char *current_arg = NULL;
 		const char *current_arg_str = NULL;
-		int br_in_0 = 0;	/* [] */
-		int br_in_1 = 0;	/* () */
 		ip++;			/* skip '(', or ',' */
-		ip1 = ip;
 		current_arg = ip;
 		current_arg_str = ip;
 		while ( *current_arg_str==' ' || *current_arg_str=='\t' )
@@ -422,17 +522,8 @@ static int add_sz_val( const char *proc_name, int target_arg,
 		if ( DEBUG ) {
 		    fprintf(stderr,"debug: current_arg = [%s]\n",current_arg);
 		}
-		while ( *ip1 != '\n' && *ip1 != '\0' ) {
-		    if ( (*ip1 == ','||*ip1 == ')') &&
-			 br_in_0 == 0 && br_in_1 == 0 ) {
-			next_arg = ip1;
-			break;
-		    }
-		    else if ( *ip1 == '[' ) br_in_0++;
-		    else if ( *ip1 == '(' ) br_in_1++;
-		    else if ( *ip1 == ']' ) br_in_0--;
-		    else if ( *ip1 == ')' ) br_in_1--;
-		    ip1 ++;
+		if ( parse_in_braces(ip, lines, j, proc_end[i]-1, ",)", &next_arg) != j ) {
+		    next_arg = NULL;
 		}
 		if ( next_arg == NULL ) {
 		    char tmp_buf[SZ_LINE_BUF];
@@ -457,10 +548,10 @@ static int add_sz_val( const char *proc_name, int target_arg,
 			goto quit;
 		    }
 		    lines[next_j][0] = '\0';
-		    j--;	/* try again */
 		    printf("[INFO] file = %s  concatenated line: %d and %d\n",
 			   file_name,j+1,next_j+1);
 		    fflush(stdout);
+		    j--;	/* try again */
 		    break;
 		}
 		if ( arg_cnt == target_arg ) {
@@ -614,6 +705,7 @@ static int add_sz_val( const char *proc_name, int target_arg,
 	}
 	free(lines);
     }
+    if ( flags != NULL ) free(flags);
     if ( fp != NULL ) fclose(fp);
     return return_status;
 }
