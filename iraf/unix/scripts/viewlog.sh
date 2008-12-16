9de10564d4ccbfb4c07e2e4c99136e81d87eaa2a
cyamauch <cyamauch@0682cb55-6b43-0410-8320-9b94b4336a91>
2008-12-16 12:39:22 +0000

    Added a simple log viewer for `make check_xxx'.
    git-svn-id: https://svn.code.sf.net/p/iraf64/code/trunk/src@356 0682cb55-6b43-0410-8320-9b94b4336a91

---
+++ iraf/unix/scripts/viewlog.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cat $1 | awk '{ if (match($0,"^[a-z0-9_][a-z0-9_]*[.][xf]:")) {SRC=$0;} else if (match($0,"^   [a-z0-9_][a-z0-9_]*:")) {FNC=substr($0,4,length($0)-4);} else if(match($1,"^[a-z/]")) {SRC="";print;} else if ($0 != "") {if (SRC != ""){printf("%s In %c%s%c:\n",SRC,96,FNC,39);print;} else {print;} }  }'
+
