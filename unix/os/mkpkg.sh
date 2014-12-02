90eb736d0a20b633cbb306b66f3cfff396dc7192
Christine Slocum <cslocum@stsci.edu>
2014-12-02 22:19:10 +0000

    more changes to mkpkg.sh (IRAF)
    git-svn-id: https://aeon.stsci.edu/ssb/svn/u-rel/iraf-2.16/trunk@3667 b5df220a-3e49-4033-9103-db9df7a1ea3e

--- unix/os/mkpkg.sh
+++ unix/os/mkpkg.sh
@@ -24,13 +24,20 @@ done
 #ar rv		libos.a *.o; ar dv libos.a zmain.o; rm *.o
 
 if test "$IRAFARCH" = "macosx"; then
+    $CC -c -O -DMACOSX -w -Wunused -arch ppc ../as/zsvjmp_ppc.s -o zsvjmp.o ;\
+    libtool -a -T -o libos.a zsvjmp.o
+    rm -f zsvjmp.o
     $CC -c -O -DMACOSX -w -Wunused -arch i386 ../as/zsvjmp_i386.s -o zsvjmp.o ;\
-    #libtool -a -T -o libos.a zsvjmp.o
-    ar r libos.a zsvjmp.o
+    libtool -a -T -o libos.a libos.a zsvjmp.o
     rm -f zsvjmp.o zmain.o
-    #libtool -a -T -o libos.a libos.a *.o
-    ar r libos.a *.o
-    ranlib libos.a
+    libtool -a -T -o libos.a libos.a *.o
+
+
+#    $CC -c -O -DMACOSX -w -Wunused -arch i386 ../as/zsvjmp_i386.s -o zsvjmp.o ;\
+#    libtool -a -T -o libos.a zsvjmp.o
+#    rm -f zsvjmp.o zmain.o
+#    libtool -a -T -o libos.a libos.a *.o
+
 
 else
     rm -f zmain.o
