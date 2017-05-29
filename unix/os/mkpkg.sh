bc981381b752faf30d03c9b65e147e11aab19e87
Ole Streicher <olebole@debian.org>
2017-05-29 10:35:52 +0200

    Clean up unix/os/mkpkg.sh
    This script was quite complicated, which lead to the problem that on
    `macosx` the `-mmacosx-version-min=10.5` was not added to the
    assemply of `zsvjamp_i386.x`. This patch simplifies the script and
    ensures the consequent use of the compiler flags.

--- unix/os/mkpkg.sh
+++ unix/os/mkpkg.sh
@@ -3,40 +3,18 @@
 echo		"--------------------- OS ----------------------"
 
 
-$CC -c $HSI_CF -Wall alloc.c getproc.c
-$CC $HSI_LF -Wall alloc.o getproc.o $HSI_OSLIBS -o alloc.e
-chmod		4755 alloc.e
-mv -f		alloc.e ../hlib
-rm -f		alloc.o
-
-
-if test "$IRAFARCH" != "macosx"; then
-    for i in zsvjmp ;\
-        do $CC -c $HSI_CF -Wall ../as/$i.s -o $i.o ;\
-    done
-fi
-
-
-for i in gmttolst.c irafpath.c prwait.c z*.c ;\
-    do $CC -c $HSI_CF -Wall $i ;\
-done
-
-#ar rv		libos.a *.o; ar dv libos.a zmain.o; rm *.o
-
+zsvjmp=../as/zsvjmp.s
 if [ "$IRAFARCH" = "macosx" ]; then
-##    $CC -c -O -DMACOSX -w -Wunused -arch ppc  ../as/zsvjmp_ppc.s  -o zsvjmp.o ;\
-##    libtool -a -T -o libos.a zsvjmp.o
-##    rm -f zsvjmp.o
-    $CC -c -O -DMACOSX -w -Wunused -m32 -arch i386 ../as/zsvjmp_i386.s -o zsvjmp.o ;\
-    ar r	libos.a *.o; 
-    ranlib	libos.a
-    rm -f 	zsvjmp.o zmain.o
-
-else
-    rm -f zmain.o
-    ar r	libos.a *.o; 
-    ranlib	libos.a
+    zsvjmp=../as/zsvjmp_i386.s
 fi
+
+$CC -c $HSI_CF -Wall $zsvjmp gmttolst.c irafpath.c prwait.c z*.c alloc.c getproc.c
+$CC $HSI_LF -Wall alloc.o getproc.o $HSI_OSLIBS -o alloc.e
+chmod	4755 alloc.e
+mv -f	alloc.e ../hlib
+rm -f	alloc.o zmain.o
+ar r	libos.a *.o; 
+ranlib	libos.a
 
 rm *.o
 mv -f		libos.a ../bin
