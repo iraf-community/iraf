87ae7bbd44d5df1587ddf9f9d0040c288463072f
Ole Streicher <olebole@debian.org>
2017-06-02 17:05:48 +0200

    Remove outdated mc6800, portkit, shlib, and sun

--- unix/shlib/zzzend.c
+++
@@ -1,8 +0,0 @@
-/* Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
- */
-
-/*
- * ZZZEND -- Dummy symbol used to mark the end of the shared image.
- * (makes debugger stack traces easier to understand).
- */
-zzzend_(){}
