f6494187f3c01b0f26152dec9b55f5f15654a21e
Ole Streicher <olebole@debian.org>
2017-06-15 14:10:51 +0200

    Remove explicite PATH settings in environment

--- unix/hlib/f77.sh
+++ unix/hlib/f77.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PATH=/v/bin:/bin:/usr/bin:/usr/local/bin
 # f77-style shell script to compile and load fortran, C, and assembly codes
 #	usage:	f77 [-g] [-O|-O[23456]] [-o absfile] [-c] files [-l library]
 #		-o objfile	Override default executable name a.out.
