9b2990430d963934ac8a3371ac05b31c86d25e29
Ole Streicher <olebole@debian.org>
2017-06-27 08:56:56 +0200

    Remove VO related vendor libs
    These libraries should be moved to a separate IRAF-VO package,
    together with the other VO packages and VOCL.

--- vendor/voclient/libvoclient/examples/simple.tcl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/tclsh
-
-load ./voclient.so voclient
-puts [voc_coneCaller "http://www.nofs.navy.mil/cgi-bin/vo_cone.cgi?CAT=USNO-B1&" 12. 12. 0.1 1]
-
--- vendor/voclient/testscripts/votopic.sh
+++
@@ -1,9 +0,0 @@
-for file in m31
-do
-	echo 
-	echo ---- $file ----
-	votopic -t catalog radio $file 0.10
-	echo 
-	echo --------------
-	echo 
-done
