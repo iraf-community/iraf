4b85135ebfa523ba05fc50821706d7017ad69c3d
Ole Streicher <olebole@debian.org>
2017-09-21 11:15:57 +0200

    Simplify ZGMTCO
    The original code is broken due to a number of assumptions taken in
    the code:
    
     * It assumes Jan 1 1980 as being without daylight saving time, which
       is not generally true on the southern hemisphere.
     * It assumes that the offset did not change (except for DST) since
       1980, which is not generally true: F.e. the time zone "Europe/Kiev"
       changed the offset from UTC + 2 hours to UTC + 1 hour after the
       seggregation of the Soviet Union.
     * It assumes that daylight saving time is always 1 hour.

--- unix/os/zgmtco.c
+++ unix/os/zgmtco.c
@@ -2,15 +2,12 @@
  */
 
 #include <stdio.h>
-#include <sys/types.h>
 #include <time.h>
 
 #define	import_kernel
 #define	import_knames
 #define import_spp
 #include <iraf.h>
-
-#define SECONDS_1970_TO_1980    315532800L
 
 
 /* ZGMTCO -- Return the correction, in seconds, from local standard time
@@ -21,29 +18,9 @@ ZGMTCO (
   XINT	*gmtcor				/* seconds */
 )
 {
-	time_t gmt_to_lst(), ltime;
-
-	/* Given an input value of zero (biased by SECONDS_1970_TO_1980)
-	 * gmt_to_lst will return a negative value in seconds for a location
-	 * in the US (as an limiting test case).  We want to return the
-	 * correction to LST to get GMT, a positive value for the US, so
-	 * we need to negate this value.  gmt_to_lst will already have taken
-	 * daylight savings time into account.  Although we talk about the
-	 * US (as a test case) this relation will hold both east and west
-	 * of Greenwich.
-	 */
-
-	*gmtcor = -((XINT) gmt_to_lst ((time_t) SECONDS_1970_TO_1980));
-
-
-        /* Daylight saving time is not added to the output of gmt_to_lst()
-	 * since it assumes Jan 1.  Use the current date to determin if
-	 * DST is in effect.
-	 */
-
-	ltime = time(0);
-	if (localtime(&ltime)->tm_isdst)
-	    *gmtcor = *gmtcor - 60L * 60L;
-
+	time_t now = time(NULL);
+	struct tm ptm;
+	localtime_r(&now, &ptm);
+	*gmtcor = -ptm.tm_gmtoff;
 	return (XOK);
 }
