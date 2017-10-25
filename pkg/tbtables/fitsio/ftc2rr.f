5430befdcf9ef9a726ffdb23b559242e433b8c5d
Ole Streicher <olebole@debian.org>
2017-10-25 11:59:40 -0300

    Add a trailing `\n` to the end of variable format strings.
    f2c does not recognize the length of a variable FORMAT string and will
    try to read the string until a non-blank appears.
    
    The format strings are declared as `CHARACTER*8` and do not
    automatically have a `\0` appended at the end. Therefore, when the
    string is filled with blanks until the end, it will read one char over
    the end (which is hopefully a non-blank).
    
    This is probably a bug in f2c; however it is not fixed in the recent
    versions as well. To work around this, a trailing `\0` is
    automatically added.

--- pkg/tbtables/fitsio/ftc2rr.f
+++ pkg/tbtables/fitsio/ftc2rr.f
@@ -22,11 +22,11 @@ C       find length of the input real character string
 
 C       construct the format statement to read the character string
         if (nleng .le. 9)then
-                write(iform,1000)nleng
-1000            format('(F',I1,'.0)')
+                write(iform,1000)nleng, '\0'
+1000            format('(F',I1,'.0)',A)
         else
-                write(iform,1001)nleng
-1001            format('(F',I2,'.0)')
+                write(iform,1001)nleng, '\0'
+1001            format('(F',I2,'.0)',A)
         end if
 
         read(cval,iform,err=900)val
