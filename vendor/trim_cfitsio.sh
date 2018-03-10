cfb6a72fa046238fc1a4d37dba8df83d67e39490
Ole Streicher <olebole@debian.org>
2018-03-10 11:14:08 +0100

    Trim cfitsio to the necessary content
    The script is adopted from astropy/cextern/trim_cfitsio.sh and removes
    everything except the library itself and the license. It also adds the
    required changes for IRAF.
    
    This should make updating to a new cfitsio version easier.

---
+++ vendor/trim_cfitsio.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+# Copyright (c) 2011-2017, Astropy Developers
+# Distributable under a BSD-3-Clause license
+
+# This script is adopted from astropy and adjusted for the needs in IRFA.
+# It should be run every time cfitsio is updated.
+# This moves all the code needed for the actual library to lib
+# and deletes everything else (except License.txt and doc/changes.txt)
+
+# So, the standard update would be to execute, from this directory,
+# rm -rf cfitsio
+# tar xvf <PATH_TO_TAR>   # (e.g., cfitsio3410.tar.gz)
+# ./trim_cfitsio.sh
+
+set -e
+
+# Locally patch wcsutil.c
+patch -s -p1 <<EOF
+--- b/cfitsio/wcsutil.c
++++ a/cfitsio/wcsutil.c
+@@ -81,8 +81,9 @@ int ffwldp(double xpix, double ypix, double xref, double yref,
+       dect = dec0 + m;
+ 
+     } else if (*cptr == 'T') {  /* -TAN */
+-      if (*(cptr + 1) != 'A' ||  *(cptr + 2) != 'N') {
+-         return(*status = 504);
++      if ( !(*(cptr + 1) == 'A' && *(cptr + 2) == 'N') && 
++           !(*(cptr + 1) == 'P' && *(cptr + 2) == 'V') ) {
++         	return(*status = 504);
+       }
+       x = cos0*cos(ra0) - l*sin(ra0) - m*cos(ra0)*sin0;
+       y = cos0*sin(ra0) + l*cos(ra0) - m*sin(ra0)*sin0;
+
+EOF
+
+# This just gets CORE_SOURCES from Makefile.in
+lib_files=`make -f cfitsio/Makefile.in cfitsioLibSrcs`
+flib_files='f77_wrap1.c f77_wrap2.c f77_wrap3.c f77_wrap4.c'
+# The include files cannot be directly inferred from Makefile.in
+inc_files='fitsio.h fitsio2.h longnam.h drvrsmem.h eval_defs.h eval_tab.h region.h group.h simplerng.h grparser.h zlib.h zconf.h zutil.h crc32.h inftrees.h inflate.h inffast.h deflate.h trees.h inffixed.h f77_wrap.h cfortran.h'
+
+if [ ! -d cfitsio/lib ]; then
+    mkdir cfitsio/lib
+fi
+
+for fil in $lib_files $flib_files $inc_files configure.ac Makefile.in; do
+    if [ -f cfitsio/$fil ]; then
+        mv cfitsio/$fil cfitsio/lib/
+    fi
+done
+rm -f cfitsio/cfitsio.xcodeproj
+rm -f cfitsio/docs/*.tex
+rm -f cfitsio/docs/*.ps
+rm -f cfitsio/docs/*.pdf
+rm -f cfitsio/docs/*.doc
+rm -f cfitsio/docs/*.toc
+rm -f cfitsio/[^L]*.*
+mv cfitsio/lib/* cfitsio/
+rmdir cfitsio/lib/
+cat <<EOF >cfitsio/README.IRAF
+Note: IRAF only requires the CFITSIO library, and hence in this bundled version,
+we removed all other files except the required license (License.txt) and changelog
+(docs/changes.txt, which has the version number).
+EOF
+cat <<EOF >cfitsio/mklibs
+#!/bin/sh
+
+top=\$(pwd)
+
+export CC=gcc
+export CXX=g++
+
+
+if [ "\$PLMACH" = "macosx" ] ; then
+   export CFLAGS="-DDarwin"
+fi
+
+echo "  (Using toplevel directory \$top ....)"
+
+# Global options.
+gopts="--prefix=\$top --exec-prefix=\$top --disable-shared"
+
+./configure \$gopts  			       | tee _spool 2>&1
+make clean 				       | tee -a _spool 2>&1
+make 					       | tee -a _spool 2>&1
+
+cp   libcfitsio.a ../voclient/lib
+mv   libcfitsio.a ../../lib
+cp   fitsio*.h longnam.h ../../include
+
+make clean
+echo "done"
+EOF
+
+chmod +x cfitsio/mklibs
