0aa37118b460f1946fc397aaecff32daf6570244
Mike Fitzpatrick <fitz@noao.edu>
2020-01-13 22:46:37 -0700

    ANSI cleanup

--- sys/ki/zzrdks.c
+++ sys/ki/zzrdks.c
@@ -1,19 +1,31 @@
 /* Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
  */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int	spoolit = 0;
 int	spoolfd = 0;
+
+extern int zardks_ (int *chan, short *buf, int *totbytes, int *loffset);
+extern int zawtks_ (int *chan, int *status);
+
 
 /* Intercept KS intput and spool in a file for subsequent debugging.
  * [MACHDEP].  This is a UNIX dependent debugging routine.  To get rid of
  * it, delete the file, edit the Makefile, and change the reference to
  * zzrdks in irafks.x to zardks.
  */
-zzrdks_ (chan, buf, maxb, off)
-int	*chan;
-short	*buf;
-int	*maxb;
-int	*off;
+void
+zzrdks_ (
+    int	  *chan,
+    short *buf,
+    int	  *maxb,
+    int	  *off
+)
 {
 	int	status;
 
