c91a5ce3a2974ab9f5d54195ced39f9b22e06e4e
Ole Streicher <olebole@debian.org>
2023-03-27 15:41:04 +0200

    Adjust mkiraf script to new login.cl

--- unix/hlib/mkiraf.sh
+++ unix/hlib/mkiraf.sh
@@ -5,123 +5,94 @@
 #
 #  Usage:
 #
-#	% mkiraf [--init] [--noinit] [--quiet]
+#	% mkiraf [--default] [--init] [--noinit] [--quiet]
 #
 #  Where
+#	-d,--default		Create default login dir
 #	-i,--init		Initialize the uparm directory
 #	-n,--noinit		Do not nitialize the uparm directory
 #	-q,--quiet		Suppress output
 #
-#  Use of the -i or -n options will suppress the corresponding prompts
+#  Use of the -i or -n options will suppress the corresponding prompt
 #  for input.
 
 
 # Initialize the script variables.
-uparm_init=-1
-quiet=0
-def=0
-
+uparm_init="ask"
+quiet=""
+def=""
 imdir="${HOME}/.iraf/imdir/"
 cachedir="${HOME}/.iraf/cache/"
 
-# Paths edited by the install script.
-iraf="/home/ole/Projects/2015/upstream/iraf/iraf/"
-
-
-
-# ------------- (end of site dependent definitions) ------------------------
-
-# The following kludge is for Solaris, which doesn't have whoami.
-if [ "$USER" = "" ]; then
-    USER=$(whoami)
-fi
-
+# Path edited by the install script.
+iraf="/iraf/iraf/"
 
 # Parse the command-line options.
-for i in "$@"
-do
-  case $i in
-    -d|--default)			# Create default login dir
-        def=1
-        quiet=1
-	echo ""
-    	;;
-    -i|--init)				# Initialize uparm directory
-        uparm_init=1
-    	;;
-    -n|--noinit)			# Don't initialize uparm directory
-        uparm_init=0
-    	;;
-    -q|--quiet)				# Suppress output
-        quiet=1
-    	;;
-    *)
-        echo "Error: unknown option '$i'"
-	exit 1
-    	;;
-  esac
+for i in "$@"; do
+    case $i in
+	-d|--default)			# Create default login dir
+            def="default"
+            quiet="quiet"
+	    echo ""
+    	    ;;
+	-i|--init)			# Initialize uparm directory
+            uparm_init="yes"
+    	    ;;
+	-n|--noinit)			# Don't initialize uparm directory
+            uparm_init="no"
+    	    ;;
+	-q|--quiet)			# Suppress output
+            quiet="quiet"
+    	    ;;
+	*)
+            echo "Error: unknown option '$i'"
+	    exit 1
+    	    ;;
+    esac
 done
 
-
-
-# Protect against running mkiraf in an iraf system directory.
-irafdir=$(cd "$iraf" ; pwd)
-if [ ! "$(pwd | grep "$irafdir")" = "" ]; then
-    if [ "$(pwd | grep iraf/local)" = "" ]; then
+# With --default, chdir to the default directory to create uparm and
+# login.cl there
+if [ "$def" ]; then
+    cd "${HOME}/.iraf/"
+else
+    # Protect against running mkiraf in an iraf system directory.
+    irafdir=$(cd "$iraf" ; pwd)
+    if (pwd | grep -q "^$irafdir") && ! (pwd | grep -q iraf/local); then
 	echo "Error: current directory is not an iraf user login directory"
 	exit 1
     fi
 fi
 
-if [ "$def" = 1 ]; then
-    imdir="$HOME/.iraf/imdir/"
-    cachedir="$HOME/.iraf/cache/"
-    cd "$HOME"
-    if [ ! -e .iraf ]; then
-	mkdir "$HOME/.iraf"
-    fi
-    cd "$HOME/.iraf"
-    if [ ! -e bin ]; then
-        mkdir bin
-    fi
-    if [ ! -e imdir ]; then
-        mkdir imdir
-    fi
-    if [ ! -e cache ]; then
-        mkdir cache
-    fi
-    cp $iraf/unix/hlib/setup.*sh .
-fi
-
+# Create imdir and cache dir if not already there
+mkdir -p "${imdir}" "${cachedir}"
 
 # Make an empty "uparm" (user parameter) directory.
 if [ ! -e uparm ]; then
-    if [ "$quiet" -lt 1 ]; then
-      if [ "$def" = 0 ]; then
+    if [ ! "$quiet" ]; then
         echo '-- creating a new uparm directory'
-      fi
     fi
     mkdir uparm
 elif [ ! -d uparm ]; then
     echo "Error: a file uparm exists"
     exit 1
 else
-    if [ "$uparm_init" -lt 0 ] ; then
-      if [ "$quiet"  -lt 1 ] ; then
-        printf 'Initialize uparm? (y|n): '
-        read yesno
-      else
-	yesno="yes"
-      fi
-      if [ "$yesno" = "y" ] || [ "$yesno" = "yes" ]; then
-	if [ "$quiet" -lt 1 ]; then
-	    echo '-- initializing uparm'
+    if [ "$uparm_init" = "ask" ] ; then
+	if [ ! "$quiet" ] ; then
+            printf 'Initialize uparm? (y|n): '
+            read yesno
+	else
+	    yesno="yes"
 	fi
-	rm -rf uparm
-	mkdir uparm
-      fi
-    elif [ "$uparm_init" = 1 ]; then
-	if [ "$quiet" -lt 1 ]; then
+	if [ "$yesno" = "y" ] || [ "$yesno" = "yes" ]; then
+	    if [ ! "$quiet" ]; then
+		echo '-- initializing uparm'
+	    fi
+	    rm -rf uparm
+	    mkdir uparm
+	fi
+    elif [ "$uparm_init" = "yes" ]; then
+	if [ ! "$quiet" ]; then
 	    echo '-- initializing uparm'
 	fi
 	rm -rf uparm
@@ -129,46 +100,24 @@ else
     fi
 fi
 
-# Edit the login.cl file, setting the user's home directory, and
-# default image directory
-
-# Initialize the 'imdir' and 'cachedir' paths.
-IDIR="${imdir}$USER"
-if [ -d "$imdir" ]; then
-    mkdir -p "$IDIR"
-fi
-if [ ! -d "$IDIR" ] || [ ! -w "$IDIR" ]; then
-    IDIR="HDR$"
-fi
-
-CDIR="${cachedir}$USER"
-if [ -d "$cachedir" ]; then
-    mkdir -p "$CDIR"
-fi
-if [ ! -d "$CDIR" ] || [ ! -w "$CDIR" ]; then
-    CDIR="/tmp"
-fi
-
-
 # Back up the old login.cl file.
 if [ -e login.cl ]; then
     mv -f login.cl login.cl.OLD
 fi
 
-# Create the path editing script.
-_sed() {
-    pwd			| sed -e "s;.*;s+U_HOME+&/+;"
-    pwd			| sed -e "s;.*;s+U_UPARM+&/uparm/+;"
-    echo "$IDIR"	| sed -e "s;.*;s+U_IMDIR+&/+;"
-    echo "$CDIR"	| sed -e "s;.*;s+U_CACHEDIR+&/+;"
-    echo "$USER"	| sed -e "s;.*;s+U_USER+&+;"
-}
+# Create local login.cl
+if [ ! "$def" ]; then
+    # Patch the "set home = ..." line in login.cl. The uparm path is
+    # derived from that.
+    RE='^[[:space:]]*set[[:space:]]*home[[:space:]]*=.*'
+    LINE="set	home		= $(pwd)/"
+    sed -E "s+${RE}+${LINE}+" "${iraf}unix/hlib/login.cl" > login.cl
+else
+    # The default dir is already set in login.cl; no need to patch
+    cp -f "${iraf}unix/hlib/login.cl" login.cl
+fi
 
-sed "$(_sed)" < "${iraf}/unix/hlib/login.cl" > login.cl
-
-if [ $def = 0 ]; then
- if [ $quiet -lt 1 ] ; then
-  echo 'A new LOGIN.CL file has been created in the current directory.'
-  echo 'You may wish to review and edit this file to change the defaults.'
- fi
+if [ ! "$def" ]; then
+    echo 'A new LOGIN.CL file has been created in the current directory.'
+    echo 'You may wish to review and edit this file to change the defaults.'
 fi
