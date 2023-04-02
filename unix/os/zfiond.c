70a292423d8c6f7095f348711b14fb516fa42b27
Ole Streicher <olebole@debian.org>
2023-04-02 14:26:02 +0200

    Use standard hp->h_addr_list[0] in zfiond

--- unix/os/zfiond.c
+++ unix/os/zfiond.c
@@ -224,7 +224,7 @@ ZOPNND (
 		    if (host_addr == INADDR_NONE)
 			goto err;
 		} else if ((hp = gethostbyname(host_str))) {
-  		    memcpy (&host_addr, hp->h_addr, sizeof(host_addr));
+ 		    memcpy (&host_addr, hp->h_addr_list[0], sizeof(host_addr));
 		} else
 		    goto err;
 
