5caa598f84d57f90cc21e1a8b54b311bc1a3a949
Ole Streicher <olebole@debian.org>
2023-04-25 17:40:14 +0200

    Add explicit patches for libf2c

--- unix/f2c/trim_f2c.sh
+++ unix/f2c/trim_f2c.sh
@@ -4,10 +4,14 @@
 set -e
 
 ################### F2C ######################
-# Base: "src" subdir of f2c-20100827.tar.gz, f.e. from the Debian
-# sources
+# Base: "src" subdir of f2c-20100827.tar.gz, f.e. from the Debian sources
 # Remove files not needed here
 rm -f src/f2c.h src/readme src/tokdefs.h src/xsum0.out src/index.html src/mkfile.plan9 
+
+################# LIBF2C ####################
+# Base: "src" subdir of libf2c2-20090411.tar.gz, f.e. from the Debian sources
+# Remove files not needed here
+rm -f libf2c/xsum0.out
 
 
 # Patch f2c and libf2c
