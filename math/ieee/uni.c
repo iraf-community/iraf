de8d3675a0d03c86eb55a720cc839a44cba5db32
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-08-22 10:43:52 -0700

    proto declarations, added <stdlib.h>

--- math/ieee/uni.c
+++ math/ieee/uni.c
@@ -1,8 +1,8 @@
 /* Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
  */
+#include <stdlib.h>
 
-float uni_(dummy)
-float *dummy;
+float uni_(float *dummy)
 {
 	return(rand()/ 2147483648.0);	/* return a real [0.0, 1.0) */
 }
