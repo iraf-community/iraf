07a453c9a6e496977ed42aa62a16fb2ed933402e
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-09-14 20:59:51 -0700

    restore import_libc

--- sys/libc/strncmp.c
+++ sys/libc/strncmp.c
@@ -1,7 +1,7 @@
 /* Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
 */
 
-//#define	import_libc
+#define	import_libc
 #include <iraf.h>
 
 /* STRNCMP -- Compare up to the first N characters of two strings.  -N is
