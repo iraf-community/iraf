74c0ab0ad43f86da4bb05a739bc9b608373f8f4a
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-09-15 19:59:12 -0700

    typo and formatting

--- unix/boot/spp/rpp/ratlibc/mkpkg.sh
+++ unix/boot/spp/rpp/ratlibc/mkpkg.sh
@@ -1,7 +1,7 @@
-# Hos system interface for the RPP program.
+# Host system interface for the RPP program.
 
-$CC -c -g $HSI_CF	cant.c close.c endst.c getarg.c getlin.c initst.c open.c\
-		putch.c putlin.c r4tocstr.c remark.c
+$CC -c -g $HSI_CF cant.c close.c endst.c getarg.c getlin.c initst.c \
+		  open.c putch.c putlin.c r4tocstr.c remark.c
 
 ar rv		libc.a *.o
 $RANLIB		libc.a
