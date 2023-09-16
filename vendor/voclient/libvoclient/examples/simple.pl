18d45b6f67315934d4af59b860d33eceb8042b85
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-09-15 18:44:36 -0700

    removed vendor and vo code

--- vendor/voclient/libvoclient/examples/simple.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/perl 
-
-use voclient;
-
-print voclient::voc_coneCaller("http://www.nofs.navy.mil/cgi-bin/vo_cone.cgi?CAT=USNO-B1&",12.,12.,0.1,2),"\n\n";
-
--- vendor/x11iraf/ximtool/clients.old/x_ism.x
+++
@@ -1,1 +0,0 @@
-task wcspix = t_wcspix
