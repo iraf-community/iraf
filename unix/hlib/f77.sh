7b226bf76db17a7b1bd7dc7167e11aec4e2b566b
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-09-15 21:12:10 -0700

    suppress warnings and force C89 std on fortran compilation

--- unix/hlib/f77.sh
+++ unix/hlib/f77.sh
@@ -32,9 +32,8 @@ PATH=/v/bin:/bin:/usr/bin:/usr/local/bin
 
 s=/tmp/stderr_$$
 t=/tmp/f77_$$
-#CC=${CC_f2c:-'/usr/bin/cc -m486'}
 CC=${CC_f2c:-'gcc'}
-CFLAGS=${CFLAGS:-"-I${iraf}unix/bin.${IRAFARCH}"}
+CFLAGS=${CFLAGS:-"-I${iraf}unix/bin.${IRAFARCH} -w --std=c89"}
 EFL=${EFL:-/v/bin/efl}
 EFLFLAGS=${EFLFLAGS:-'system=portable deltastno=10'}
 F2C=${F2C:-/usr/bin/f2c}
