3b15ab0450a3596e5a7a03667199fd57aeadeb27
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-09-28 22:25:11 -0700

    added comments

--- unix/as.macosx/zsvjmp.s
+++ unix/as.macosx/zsvjmp.s
@@ -1,22 +1,31 @@
-# 1 "zz.S"
-# 1 "<built-in>" 1
-# 1 "zz.S" 2
-# 32 "zz.S"
- .file "zsvjmp.S"
+        .file "zsvjmp.S"
 
+# ZSVJMP, ZDOJMP -- Set up a jump (non-local goto) by saving the processor
+# registers in the buffer jmpbuf.  A subsequent call to ZDOJMP restores
+# the registers, effecting a call in the context of the procedure which
+# originally called ZSVJMP, but with the new status code. These are Fortran
+# callable procedures.
+#
+#               zsvjmp (jmp_buf, status)        # (returns status)
+#               zdojmp (jmp_buf, status)        # (passes status to zsvjmp)
+#
+# These routines are directly comparable to the UNIX setjmp/longjmp, except
+# that they are Fortran callable kernel routines, i.e., trailing underscore,
+# call by reference, and no function returns.  ZSVJMP requires an assembler
+# jacket routine to avoid modifying the call stack, but relies upon setjmp
+# to do the real work.  ZDOJMP is implemented as a portable C routine in OS,
+# calling longjmp to do the restore.  In these routines, JMP_BUF consists
+# of one longword containing the address of the STATUS variable, followed
+# by the "jmp_buf" used by setjmp/longjmp.
 
  .arch armv8-a
-# 44 "zz.S"
  .align 2
-
-
         .globl _zsvjmp_
- .text
+
+        .text
 _zsvjmp_:
-# 80 "zz.S"
- str xzr, [x1]
- str x1, [x0], 8
+        str xzr, [x1]           // zero the value of status
+        str x1, [x0], 8         // change stack to point to &jmpbuf[1]
+        mov w1, 0               // change arg2 to zero
 
- mov w1, 0
-
- b _sigsetjmp
+        b _sigsetjmp            // let sigsetjmp do the rest
