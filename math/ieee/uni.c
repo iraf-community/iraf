aa674fa0f08ed3598488a5d3053f60473743c586
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2023-11-26 21:10:10 -0700

    removed unused ieee code

--- math/ieee/uni.c
+++
@@ -1,8 +0,0 @@
-/* Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
- */
-#include <stdlib.h>
-
-float uni_(float *dummy)
-{
-	return(rand()/ 2147483648.0);	/* return a real [0.0, 1.0) */
-}
