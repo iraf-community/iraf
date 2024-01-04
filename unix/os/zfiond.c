d8226de5e5514a50767582b46f222ad09256b777
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2024-01-04 15:56:55 -0700

    fix compiler warnings from linux64

--- unix/os/zfiond.c
+++ unix/os/zfiond.c
@@ -707,7 +707,7 @@ ZARDND (
 #endif
 
 	/* Determine maximum amount of data to be read. */
-	maxread = (np->flags & F_TEXT) ? *maxbytes/sizeof(XCHAR) : *maxbytes;
+	maxread = (np->flags & F_TEXT) ?  (int)(*maxbytes/sizeof(XCHAR)) : (int)*maxbytes;
 
 	/* The following call to select shouldn't be necessary, but it
 	 * appears that, due to the way we open a FIFO with O_NDELAY, read
@@ -836,8 +836,9 @@ nd_onsig (
         /* If we get a SIGPIPE writing to a server the server has probably
          * died.  Make it look like there was an i/o error on the channel.
          */
-        if (sig == SIGPIPE && recursion++ == 0)
+        if (sig == SIGPIPE && recursion++ == 0) {
             ;
+        }
 
         if (jmpset)
             longjmp (jmpbuf, sig);
