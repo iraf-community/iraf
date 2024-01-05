760467826efdb15fca9f2000ed178451ca79b4a4
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2024-01-04 23:14:43 -0700

    ensure ADDR_TO_LOC uses unsigned addresses

--- unix/hlib/libc/kernel.h
+++ unix/hlib/libc/kernel.h
@@ -48,7 +48,7 @@
  * convert host pointer addresses (in bytes) to/from iraf pointer values
  * in units of XCHAR.
  */
-#define	ADDR_TO_LOC(addr) 	(((XINT)((XCHAR *)(addr)))>>(sizeof(XCHAR)-1))
+#define	ADDR_TO_LOC(addr) 	(((unsigned XINT)((XCHAR *)(addr)))>>(sizeof(XCHAR)-1))
 #define	LOC_TO_ADDR(loc,type)   ((type *)((XCHAR *)((loc)<<(sizeof(XCHAR)-1))))
 
 
