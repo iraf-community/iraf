388bcfc076ab093713adf4519acf6bb9f8188683
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2024-01-14 22:26:48 -0700

    cleanup

--- unix/as.macintel/zsvjmp_demo.c
+++ unix/as.macintel/zsvjmp_demo.c
@@ -5,13 +5,15 @@
  *  need to execute basically these three lines of code but in the context of
  *  the routine calling the zsvjmp, so the stack needs to be adjusted 
  *  accordingly (i.e. the assembler from this code WILL NOT work since the
- *  ZDOJMP will return here, and not the parent routine).
+ *  ZDOJMP will return here, and not the parent routine).  The adjustment
+ *  then is to change the return stack to point to jmpbuf[1] and zero the
+ *  second argument of the jmp_buf env.
  */
 
 void
 zsvjmp_ (int *buf, int *status)
 {
- 	*status = 0;
-	buf[0] = *status;
-	setjmp (&buf[1]);
+ 	*status = 0;            // zero the return status value
+	buf[0] = status;        // store address of status in jmp_buf[0]
+	setjmp (&buf[1]);       // use setjmp to return to jmp_buf[1]
 }
