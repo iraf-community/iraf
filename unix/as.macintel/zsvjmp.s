45fac76ebd392b0507c7b0d3ca9b0f393015d4d0
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2024-01-14 22:27:01 -0700

    make it more obvious

--- unix/as.macintel/zsvjmp.s
+++ unix/as.macintel/zsvjmp.s
@@ -1,5 +1,5 @@
-	.file	"zsvjmp.s"
-
+# Copyright(c) 1986 Association of Universities for Research in Astronomy Inc.
+#
 # ZSVJMP, ZDOJMP -- Set up a jump (non-local goto) by saving the processor
 # registers in the buffer jmpbuf.  A subsequent call to ZDOJMP restores
 # the registers, effecting a call in the context of the procedure which
@@ -17,29 +17,18 @@
 # calling longjmp to do the restore.  In these routines, JMP_BUF consists
 # of one longword containing the address of the STATUS variable, followed
 # by the "jmp_buf" used by setjmp/longjmp.
-#
-# This file contains the FreeBSD (x86) version of ZSVJMP.
-# Modified to remove leading underscore for ELF (Jan99).
- 
+
+	.file	"zsvjmp.s"
+
         .globl	_zsvjmp_
 
-	# The following has nothing to do with ZSVJMP, and is included here
-	# only because this assembler module is loaded with every process.
-	# This code sets the value of the symbol MEM (the VOS or Fortran Mem
-	# common) to zero, setting the origin for IRAF pointers to zero
-	# rather than some arbitrary value, and ensuring that the MEM common
-	# is aligned for all datatypes as well as page aligned.  A further
-	# advantage is that references to NULL pointers are likely to cause a
-	# memory violation.
-
-	#.globl	_mem_
-	#_mem_	=	0
-
 _zsvjmp_:
-	# %rsi ... &status  %rdi ... &jumpbuf
-	movq    %rsi, (%rdi)    # store &status in jmpbuf[0]
-	movl    $0, (%rsi)      # zero the value of status
-	addq    $8, %rdi        # change point to &jmpbuf[1]
-	movl    $0, %esi        # change arg2 to zero
-	jmp     _sigsetjmp     # let sigsetjmp do the rest
+	movq	%rdi, -8(%rbp)          # %rdi contains &jmpbuf
+	movq	%rsi, -16(%rbp)         # %rsi contains &status
+	movl    $0, (%rsi)              # zero the value of status
+	movq    %rsi, (%rdi)            # store address of status in jmpbuf[0]
+	addq    $8, %rdi                # change &jmpbuf[0] to &jmpbuf[1]
+	movl    $0, -16(%rbp)           # zero the second argument of jmpbuf
+
+	jmp     _sigsetjmp              # use sigsetjmp to do the rest
 
