c461f08aecc87cfa8bb4629e99d435185ece3818
Mike Fitzpatrick <mjfitzpatrick@gmail.com>
2025-07-30 15:06:02 -0700

    made core arch a requirement for build (again)

--- unix/hlib/mkpkg.sh
+++ unix/hlib/mkpkg.sh
@@ -50,15 +50,19 @@ if [ -e "./bin" ]; then
 fi
 
 
-# Check the core system binary configuration and throw an error if we're in 
+# Check the core system binary configuration and ensure it matches the current
+# arch to be compiled.  For external packages that may link against NOAO
+# libs like libasttools/libsmw the core arch needs to be defined to resolve
+# the links correctly if pkglibs isn't set properly. This should probably be
+# addressed in the mkpkg sources but we'll simply deal with the error here.
+
+throw an error if we're in 
 # the IRAF root.  It isn't otherwise an error to compile a package if the 
 # core system isn't configure for the current arch.
 
 c_arch=$(ls -l ${iraf}/bin | cut -d '>' -f 2 | sed -e "s/ bin\.//g")
 if [ "${c_arch}" != "$IRAFARCH" ]; then
-    if [ $iraf == `pwd`/ ]; then
-        err=1
-    fi
+    err=1
 fi
 
 if [ $err -eq 1 ]; then
